7483fc3f722279bc6c00c297eb7ede72215c34f7
Bennett Benedict <bennybenester@gmail.com>
2022-10-13 21:46:50 +0300

    add class validator and class transformer to validate request payloads

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT);
   Logger.debug(`App listening on PORT: ${PORT}`, 'APP');
 }
